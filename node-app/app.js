5818719f97146726fade516aacbae44ebb148fbe
Joshua Schmidt <jschmidtnj@gmail.com>
2018-06-09 13:14:12 -0400

    testing mongodb addition

--- node-app/app.js
+++ node-app/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
 
 // Connect To Database (NEW) But not working!!!!!!!!!! (because of secret in db.js!!!!!)
 //const db = require('./config/database');
@@ -31,6 +33,11 @@ mongoose.connection.on('error', (err) => {
 
 const app = express();
 
+//Dev:
+app.use(morgan('dev')); // log every request to the console
+
+//Routes:
+require('./routes/routes.js')(app);
 const users = require('./routes/users');
 
 // Port Number
@@ -43,7 +50,10 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -63,6 +73,7 @@ app.get('*', (req, res) => {
 });
 
 // Start Server
+// listen (start app with npm start) ======================================
 app.listen(port, () => {
   console.log('Server started on port '+port);
 });
