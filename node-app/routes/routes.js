6c3a61e63225d6533bbd76f12f8e0f95bc9d7cf9
Joshua Schmidt <jschmidtnj@gmail.com>
2018-06-09 13:19:18 -0400

    fixed one bug hopefully

--- node-app/routes/routes.js
+++ node-app/routes/routes.js
@@ -50,8 +50,10 @@ module.exports = function (app) {
         });
     });
 
+    /*
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '../angular-src/src/app/components/dashboard/dashboard.component.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
+    */
 };
